8d31141e8f4af84a92900b2fb3aa36d6f735a4a5
uiovasot <esp32chess@gmail.com>
2024-11-22 09:43:54 +0900

    setup skin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,45 +2,45 @@ const colors = require('tailwindcss/colors');
 const ColorConverter = require('./ColorConverter');
 
 const baseColors = {
-    brand: '#ee4546',
-    secondary: '#25262b',
-    dark: '#343a40',
-    light: '#f8f9fa',
+	brand: '#ee4546',
+	secondary: '#25262b',
+	dark: '#343a40',
+	light: '#f8f9fa'
 };
 
 const baseTextColors = {
-    background: '#ffffff',
-    'brand-text': '#ffffff',
-    'dark-text': '#ffffff',
-    'light-text': '#000000',
-    'secondary-text': '#ffffff',
+	background: '#ffffff',
+	'brand-text': '#ffffff',
+	'dark-text': '#ffffff',
+	'light-text': '#000000',
+	'secondary-text': '#ffffff'
 };
 
 const generateShades = (colors) => {
-    const shades = {};
-    Object.keys(colors).forEach((key) => {
-        shades[key] = {
-            DEFAULT: colors[key],
-        };
-        for (let i = 100; i <= 900; i += 100) {
-            shades[key][i] = ColorConverter.createColorForShade(colors[key], i);
-        }
-    });
-    return shades;
+	const shades = {};
+	Object.keys(colors).forEach((key) => {
+		shades[key] = {
+			DEFAULT: colors[key]
+		};
+		for (let i = 100; i <= 900; i += 100) {
+			shades[key][i] = ColorConverter.createColorForShade(colors[key], i);
+		}
+	});
+	return shades;
 };
 
 const colorsWithShades = {
-    ...generateShades(baseColors),
-    ...generateShades(baseTextColors),
+	...generateShades(baseColors),
+	...generateShades(baseTextColors)
 };
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./app/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            colors: colorsWithShades,
-        },
-    },
-    plugins: [require('@tailwindcss/typography')],
+	content: ['./app/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: colorsWithShades
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
 };
